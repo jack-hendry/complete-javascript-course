17da56500545d8b259ba883eb229f12db00a10fc
jack-hendry <jack.r.hendry.p@gmail.com>
2024-04-11 08:33:41 -0400

    289: Loading a Recipe from API

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,39 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function() {
+
+  try{
+    // 1) loading recipe
+    const res = await fetch(
+      // 'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886',
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc990'
+    );
+
+    const data = await res.json(); 
+    console.log(res, data);
+    let {recipe} = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      imge: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredints: recipe.ingredints
+    }    
+    console.log(recipe);
+    
+    if(!res.ok) throw new Error(`${data.message} (${res.status})`);
+
+    
+
+  } catch (err) {
+    alert(err); 
+  }
+}
+
+
+showRecipe()
